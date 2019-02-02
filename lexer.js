ba7113392ab16364019a46d3dea67e7b911aaa61
Lord Jerry <lovinjerry004@gmail.com>
2019-02-02 20:44:58 +0100

    add support for identifying singleLineComment, also fixed some comments typos and added some configuration

--- lexer.js
+++ lexer.js
@@ -1,27 +1,35 @@
-/** TODO: should make lexer ignore spaces
- * TODO: should make lexer igonre comments
-**/
-class lexer {
+/* eslint-disable no-console */
+
+/**
+ * * ### AFRILANG LEXER
+ * #### NOTE:
+ * * gsg
+ * #### TODO:
+ * * should make lexer ignore spaces
+ * #### TODO:
+ * * should make lexer ignore comments
+ */
+class Lexer {
   constructor(code) {
-    //input source code character
+    // input source code character
     this.code = code;
-    //lexer position
+    // lexer position
     this.line = 1;
     this.column = 1;
     this.lastPosition = -1;
-    //identifiers
+    // identifiers
     this.identifierChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    this.operator = '=;+-><';
-    this.keyWord = ['var', 'let', 'const', 'if', 'else'];
     this.numberChar = '1234567890';
-
+    this.operator = '=+-><';
+    this.separator = ',;{}[]()';
+    this.keyWord = ['var', 'let', 'fun', 'const', 'if', 'else'];
   }
 
   /**
-   * consumes character at current position and increments
+   * * consumes character at current position and increments
    * lastPosition and column
    * @return {string}
-   **/
+   * */
   eatChar() {
     this.column += 1;
     this.lastPosition += 1;
@@ -31,105 +39,101 @@ class lexer {
   /**
    * returns character at current location
    * @return {string || null}
-   **/
+   */
   peekChar() {
-    //check if we have gotten to the last character in the 
+    // check if we have gotten to the last character in the
     // input source code
-    return (this.lastPosition + 1 < this.code.length) ?
-      this.code.charAt(this.lastPosition + 1) :
-      null;
+    return (this.lastPosition + 1 < this.code.length)
+      ? this.code.charAt(this.lastPosition + 1)
+      : null;
   }
 
-  /**  
-   * check if we have gotten to te last character in 
+  /**
+   * check if we have gotten to the last character in
    * the input source code
    * @return {boolean}
-   **/
+   * */
   lastReached() {
-    return (this.lastPosition + 1 >= this.code.length) ? true : false;
-
+    return (this.lastPosition + 1 >= this.code.length);
   }
 
   /**
    * check if characters in position are valid keywords or identifier
    * @return {object || null}
-   **/
+   * */
   identifyKeyword() {
-    let char = "";
+    let char = '';
 
-    //loop through source code characters and add them up 
-    //if there are valid identifierChar
+    // loop through source code characters and add them up
+    // if there are valid identifierChar
     while (this.identifierChar.indexOf(this.peekChar()) > -1) {
       char += this.eatChar();
     }
-     //console.log(char,2)
-    //check if added character is a valid inbuilt keyword
+
+    // check if added character is a valid inbuilt keyword
     if (this.keyWord.indexOf(char) > -1) {
-      //console.log(char)
       return {
         type: 'keyword',
-        value: char
+        value: char,
       };
-    //if it is not a keyword then it has to be an identifier (variable or function name)
-    } else if (char.length > 0) {
+      // if it is not a keyword then it has to be an identifier (variable or function name)
+    }
+    if (char.length > 0) {
       return {
         type: 'identifier',
-        value: char
-      }
+        value: char,
+      };
     }
 
     return null;
-    //this.eatChar();
-
+    // this.eatChar();
   }
 
   /**
    * TODO: should probably throw an error if string is not enclosed in a quote or probably not
    * check if characters in position are valid string
    * @return {object || null}
-   **/
+   * */
   stringLiteral() {
-    let char = "";
+    let char = '';
 
-    //check if first character begins with quotes 
+    // check if first character begins with quotes
     if (this.peekChar() === "'" || this.peekChar() === '"') {
       char += this.eatChar();
 
-      //loop through source code characters and add them up 
-      //if they are enclosed in quotes
+      // loop through source code characters and add them up
+      // if they are enclosed in quotes
       while (this.peekChar() !== char[0]) {
-         
-        //used the below line to fix infinite loop, if the supposed
-        //string doesnt have a closing quote 
-        if (this.lastReached() === true) throw new SyntaxError('invalide token')
+        // used the below line to fix infinite loop, if the supposed
+        // string doesnt have a closing quote
+        if (this.lastReached() === true) throw new SyntaxError('invalide token');
 
         char += this.eatChar();
       }
 
-      //check if string character ends with quote matching the first one
+      // check if string character ends with quote matching the first one
       if (this.peekChar() === char[0]) {
         char += this.eatChar();
       }
-    } //else this.eatChar();
+    } // else this.eatChar();
 
     if (char.length > 0) {
       return {
-        type: "stringLiteral",
-        value: char
+        type: 'stringLiteral',
+        value: char,
       };
     }
     return null;
-
   }
-  
+
   /**
    * checks if characters in position are valid integers
-   * @return{object || null}
-  **/
+   * @return {object || null}
+   * */
   numberLiteral() {
-    let char = "";
-    
-    //if characters are valid numberLiterals continue consuming them
+    let char = '';
+
+    // if characters are valid numberLiterals continue consuming them
     while (this.numberChar.indexOf(this.peekChar()) > -1) {
       char += this.eatChar();
     }
@@ -137,57 +141,80 @@ class lexer {
     if (char.length > 0) {
       return {
         type: 'numberLiteral',
-        value: char
-      }
+        value: char,
+      };
     }
 
     return null;
   }
 
   /**
-   * checks if characters are valid boolean literals 
-   * @return{object || null}
-  **/
+   * checks if characters are valid boolean literals
+   * @return {object || null}
+   * */
   booleanLiteral() {
-    const { lastPosition, column, line } = this;
+    const {
+      lastPosition,
+      column,
+      line,
+    } = this;
     let char = '';
 
-    //if characters are valid charliterals add them up
+    // if characters are valid charliterals add them up
     while (this.identifierChar.indexOf(this.peekChar()) > -1) {
-      char += this.eatChar()
+      char += this.eatChar();
     }
 
-    //check if added values are boolean values
+    // check if added values are boolean values
     if (char === 'true' || char === 'false') {
       return {
         type: 'booleanLiteral',
-        value: char
-      }
+        value: char,
+      };
     }
     this.revert(lastPosition, column, line);
 
     return null;
   }
-  
+
   /**
    * checks if characters in position are valid operators
-   * @return{object || null}
-  **/
+   * @return {object || null}
+   * */
   identifyOperator() {
-    let char = "";
+    let char = '';
 
-    //if characters are valid symbols, continue consuming
+    // if characters are valid symbols, continue consuming
     while (this.operator.indexOf(this.peekChar()) > -1) {
       char += this.eatChar();
     }
 
     if (char.length > 0) {
-
       return {
-        type: "operator",
-        value: char
-      }
+        type: 'operator',
+        value: char,
+      };
+    }
 
+    return null;
+  }
+
+  /**
+   *
+   * */
+  identifySeparator() {
+    let char = '';
+
+    // if characters are valid separators, continue consuming
+    if (this.separator.indexOf(this.peekChar()) > -1) {
+      char += this.eatChar();
+    }
+
+    if (char.length > 0) {
+      return {
+        type: 'separator',
+        value: char,
+      };
     }
 
     return null;
@@ -195,10 +222,10 @@ class lexer {
 
   /**
    * checks if character in postion are newlines
-   * @return{object || null}
-  **/
+   * @return {object || null}
+   * */
   identifyNewline() {
-    let char = ''
+    let char = '';
 
     if (this.peekChar() === '\n') {
       char += this.eatChar();
@@ -206,74 +233,98 @@ class lexer {
 
       return {
         type: 'newline',
-        value: char
-      }
+        value: char,
+      };
     }
 
-    return null
+    return null;
   }
-  
+
   /**
    * checks if character in position are whitespaces
-   * @return{object || null}
-  **/
+   * @return {object || null}
+   * */
   space() {
-    let char = ""
+    let char = '';
+
     while (this.peekChar() === ' ') {
       char += this.eatChar();
     }
 
     if (char.length > 0) {
-
       return {
         type: 'space',
-        value: char
-      }
+        value: char,
+      };
     }
 
     return null;
-
   }
+
+  /**
+   * * check if characters are single line comments
+   *  @return {object || null}
+   */
+  identifySingleLineComment() {
+    let chars = '';
+
+    if (this.code.charAt(this.lastPosition + 1) === '/'
+      && this.code.charAt(this.lastPosition + 2 === '/')) {
+      chars += this.eatChar();
+      chars += this.eatChar();
+
+      while (this.peekChar() !== '\n') {
+        chars += this.eatChar();
+      }
+    }
+
+    if (chars.length > 0) {
+      return {
+        type: 'singleLineComment',
+        value: chars,
+      };
+    }
+    return null;
+  }
+
 
   /**
    * reverts code position to previous state if lexing fails
    *
-  **/
+   * */
   revert(lastPosition, column, line) {
     this.lastPosition = lastPosition;
     this.column = column;
     this.line = line;
   }
 
-  test() {
-    let token = [];
+  lex() {
+    const token = [];
 
     while (this.lastReached() !== true) {
-      //console.log(this.lastPosition+1);
-      const temp = this.booleanLiteral() ||
-        this.identifyKeyword() ||
-        this.stringLiteral() ||
-        this.identifyOperator() ||
-        this.space() ||
-        this.numberLiteral() ||
-        this.identifyNewline();
+      // console.log(this.lastPosition+1);
+      const temp = this.booleanLiteral()
+        || this.identifyKeyword()
+        || this.stringLiteral()
+        || this.identifyOperator()
+        || this.identifySeparator()
+        || this.identifySingleLineComment()
+        || this.space()
+        || this.numberLiteral()
+        || this.identifyNewline();
 
       if (temp) {
-
-        if (temp.type !== 'space' && temp.type !== 'newline'){
+        if (temp.type !== 'space' && temp.type !== 'newline') {
           token.push(temp);
         }
-
       }
-
     }
 
     return token;
   }
-
-} 
-console.time('lex')
-const code = 'true var str = "hey43+jhdh" \n; \n\n\n var num = false;';
-const lex = new lexer(code);
-console.log(lex.test());
-console.timeEnd('lex')
+}
+console.time('lex');
+const code = 'fun main(a,b){print(\'hello world\')}//heygd \n';
+const lex = new Lexer(code);
+console.log(lex.lex());
+console.timeEnd('lex');
