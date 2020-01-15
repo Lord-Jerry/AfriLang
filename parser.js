aaf65819e79712081593824f36af00af954d70a5
Lord Jerry <lovinjerry004@gmail.com>
2020-01-15 20:54:09 +0100

    implement methods for peeking and consuming tokens

--- parser.js
+++ parser.js
@@ -11,6 +11,21 @@ class Parser {
   isBound() {
     return this.position <= this.token.length - 1;
   }
+
+  peekToken() {
+    return this.isBound() ? this.token[this.position] : null;
+  }
+
+  eatToken() {
+    this.position += 1;
+    return this.token[this.position - 1];
+  }
+
+  parse() {
+    while (this.isBound()) {
+      console.log(this.eatToken());
+    }
+  }
 }
 
 module.exports = Parser;
