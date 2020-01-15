0bdb872c7f49fd3e1b3a77253fcb3e7e0123dd5c
Lord Jerry <lovinjerry004@gmail.com>
2020-01-15 20:49:37 +0100

    implement method for checking if token length has not been exceeded

--- parser.js
+++ parser.js
@@ -1,5 +1,16 @@
 class Parser {
+  constructor(token) {
+    this.token = token;
+    this.position= 0;
+    this.ast = {};
+  }
 
+  /**
+   * check if we yet to exceed token length
+   */
+  isBound() {
+    return this.position <= this.token.length - 1;
+  }
 }
 
 module.exports = Parser;
